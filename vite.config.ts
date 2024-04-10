29b8ac3ec84358ea87585fdd1744bcd5b652a74b
Carter Jackson <carterjackson96@gmail.com>
2024-04-10 17:33:55 -0400

    docker dev server

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    port: 8080,
     proxy: {
       "/api": {
         target: "http://localhost:3000",
