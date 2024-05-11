3a2ebf16ed08ec4459b169c3cf772dc0921d5909
Carter Jackson <carterjackson96@gmail.com>
2024-05-11 12:50:12 -0400

    basic vote flow

--- src/components/views/surveys/ViewSurvey.tsx
+++ src/components/views/surveys/ViewSurvey.tsx
@@ -1,13 +1,15 @@
 import { useApi } from "@/components/ApiContext";
+import { Button } from "@/components/ui/Button";
 import { useState, useEffect, useCallback } from "react";
 import { useParams } from "wouter";
 
 export function ViewSurvey() {
 	const routeParams = useParams();
-  const { apiGet } = useApi();
-  // const [loading, setLoading] = useState(false);
+  const { apiGet, apiPost } = useApi();
 	const [survey, setSurvey] = useState<any>(null);
-  const [surveyOptions, setSurveyOptions] = useState<string[]>([]);
+  const [surveyOptions, setSurveyOptions] = useState<any[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  // TODO: loading state
 
 	const fetchSurvey = useCallback(async (id: number) => {
 		const surveyResp = await apiGet(`/surveys/${id}`);
@@ -16,8 +18,40 @@ export function ViewSurvey() {
 
   const fetchSurveyOptions = useCallback(async (surveyId: number) => {
     const optionsResp = await apiGet(`/surveys/${surveyId}/options`);
-    setSurveyOptions(optionsResp as string[]);
+    setSurveyOptions(optionsResp as any[]);
   }, [apiGet]);
+
+  const handleMoveOptionUp = (optionsIndex: number) => {
+    if (optionsIndex == 0) { return; }
+    const newOptions = [...surveyOptions];
+    newOptions[optionsIndex - 1] = surveyOptions[optionsIndex];
+    newOptions[optionsIndex] = surveyOptions[optionsIndex - 1];
+    setSurveyOptions(newOptions);
+  };
+
+  const handleMoveOptionDown = (optionsIndex: number) => {
+    if (optionsIndex == surveyOptions.length - 1) { return; }
+    const newOptions = [...surveyOptions];
+    newOptions[optionsIndex + 1] = surveyOptions[optionsIndex];
+    newOptions[optionsIndex] = surveyOptions[optionsIndex + 1];
+    setSurveyOptions(newOptions);
+  };
+
+  const handleSubmit = async () => {
+    console.log(surveyOptions);
+    try {
+      await apiPost(`/surveys/${survey.id}/vote`, {
+        options: surveyOptions.map(o => o.id),
+      });
+    } catch (err) {
+      if (typeof(err) === 'string') {
+        setError(err);
+      } else {
+        console.error(err);
+        setError('Something went wrong');
+      }
+    }
+  };
 
 	// TODO: Fix double fetch
 	useEffect(() => {
@@ -32,24 +66,21 @@ export function ViewSurvey() {
 		}
 	}, [fetchSurvey, fetchSurveyOptions, routeParams?.id, survey]);
 
-  // const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   setLoading(true);
-  //   try {
-  //     await apiPut(`/surveys/${survey.id}`, { options: surveyOptions });
-  //   } catch (err) {
-  //     console.error(err);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
-
+  // TODO: Use form
   return (
     <div>
-			survey view {survey?.title}
+      <h1 className="mb-4">{survey?.title}</h1>
+      {error && <p className="text-red-800 mb-4 first-letter:uppercase">{error}</p>}
+      {/* TODO: show results */}
       {surveyOptions.map((option, index) => (
-        <div key={index}>{option}</div>
+        <div key={index} className="[&>*]:mb-2 flex items-center">
+          <Button onClick={() => handleMoveOptionUp(index)} className="mr-1">Up</Button>
+          <Button onClick={() => handleMoveOptionDown(index)} className="mr-2">Down</Button>
+          <div>{option.title}</div>
+        </div>
       ))}
+      <div className="font-semibold my-4">You will not be able to change your submission once you vote.</div>
+      <Button onClick={handleSubmit}>Vote</Button>
 		</div>
   );
 }
