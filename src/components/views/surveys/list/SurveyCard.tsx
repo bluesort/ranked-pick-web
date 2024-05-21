509b9df988188212b19c3f1c92514e0959f98238
Carter Jackson <carterjackson96@gmail.com>
2024-05-21 13:11:07 -0400

    shorter survey list cards

--- src/components/views/surveys/list/SurveyCard.tsx
+++ src/components/views/surveys/list/SurveyCard.tsx
@@ -3,7 +3,7 @@ import { Link } from "wouter";
 
 export function SurveyCard({ survey, actionSlot }: { survey: any, actionSlot: React.ReactNode }) {
 	return (
-		<div className="h-32 bg-gray-100 my-4 flex items-center justify-between p-4">
+		<div className="h-30 bg-gray-100 my-4 flex items-center justify-between p-4">
 			<div className="truncate h-full py-3">
 				<Link to={`~/surveys/${survey?.id}`} className="text-lg">{survey?.title}</Link>
 				<div className="mt-2"><ResponseCount responseCount={survey?.response_count} /></div>
