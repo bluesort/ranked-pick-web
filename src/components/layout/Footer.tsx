c0ac5ec0d93b1d2c02aae5cd728f4fad6de584c1
bluesort <carterjackson96@gmail.com>
2024-07-06 16:05:40 -0400

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
 	return (
 		<footer className="flex justify-center bg-muted text-muted-foreground h-12 p-4 text-xs [&>*]:mx-2">
-			<a href="https://github.com/carterjackson/ranked-pick-api" target="_blank">API Source</a>
-			<a href="https://github.com/carterjackson/ranked-pick-web" target="_blank">Client Source</a>
+			<a href="https://github.com/bluesort/ranked-pick-api" target="_blank">API Source</a>
+			<a href="https://github.com/bluesort/ranked-pick-web" target="_blank">Client Source</a>
 			<a href="/terms" target="_blank">Terms</a>
 			<a href="/privacy" target="_blank">Privacy</a>
 		</footer>
